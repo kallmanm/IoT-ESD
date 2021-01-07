ea0a624403f614da496c325c205f939e78ad412f
Mattias Kallman <33825140+kallmanm@users.noreply.github.com>
2021-01-07 16:20:30 +0200

    small update

--- SensorManager.py
+++ SensorManager.py
@@ -1,11 +1,5 @@
-import time
-import yaml
-from sensirion_shdlc_driver import ShdlcSerialPort, ShdlcConnection
-from sensirion_shdlc_sensorbridge import SensorBridgePort, \
-    SensorBridgeShdlcDevice, SensorBridgeI2cProxy
-from sensirion_i2c_driver import I2cConnection
-from sensirion_i2c_svm40 import Svm40I2cDevice
-
+from sensors import sps30
+import argparse
 
 # with open('sensors.yaml') as f:
 #    data = yaml.load(f, Loader=yaml.FullLoader)
@@ -14,34 +8,9 @@ from sensirion_i2c_svm40 import Svm40I2cDevice
 #    scd30_spec = data["sensors"]["scd30"]
 #    print(svm30_spec)
 #    print(scd30_spec)
+device_port = "/dev/ttyUSB0"
 
-
-class SensorManager:
-    sensor_power = False
-
-    def __init__(self, config):
-        self.config = config
-
-    def check_sensor_status(self):
-        if self.sensor_power:
-            return True
-        else:
-            return False
-
-    def power_on(self):
-        self.sensor_power = True
-        print(f'The power is {self.sensor_power}')
-        # run functionality to turn on sensor if needed.
-
-    def power_off(self):
-        self.sensor_power = False
-        print(f'The power is {self.sensor_power}')
-        # run functionality to turn off sensor if needed.
-
-    def collect_data(self):
-        try:
-            if self.check_sensor_status():
-                print('random data atm')
-
-        except RuntimeError as e:
-            print(f'{e}')
+sensor_sps30 = sps30.Sps30(device_port)
+sensor_sps30.start()
+sensor_sps30.read()
+sensor_sps30.stop()
--- sensors/sps30.py
+++ sensors/sps30.py
@@ -4,16 +4,19 @@ import serial, struct, time
 class Sps30:
     def __init__(self, port):
         self.port = port
-        self.serial = serial.Serial(self.port, baudrate=115200, stopbits=1, parity="N", timeout=2)
+        self.ser = serial.Serial(self.port, baudrate=115200, stopbits=1, parity="N", timeout=2)
 
     def start(self):
         # start command for sensor
+        print('start')
         pass
 
     def stop(self):
         # stop command for sensor
+        print('stop')
         pass
 
     def read(self):
         # reads data from sensor
+        print('read')
         pass
