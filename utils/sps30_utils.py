f9f7cabcc7a3f3cd367dbd306100b236d629c686
Mattias Kallman <33825140+kallmanm@users.noreply.github.com>
2021-01-11 11:39:41 +0200

    formatting

--- utils/sps30_utils.py
+++ utils/sps30_utils.py
@@ -8,10 +8,11 @@ def calculate_checksum(sum_of_bytes):
     The checksum is built before byte-stuffing and checked after removing stuffed bytes from the frame. The checksum
     is defined as follows:
     1. Sum all bytes between start and stop (without start and stop bytes).
-    2. Take the least
-    significant byte of the result and invert it. This will be the checksum. For a MOSI frame use Address, Command,
-    Length and Data to calculate the checksum. For a MISO frame use Address, Command, State, Length and Data to
-    calculate the checksum. :param sum: :return checksum:
+    2. Take the least significant byte of the result and invert it. This will be the checksum.
+    For a MOSI frame use Address, Command, Length and Data to calculate the checksum.
+    For a MISO frame use Address, Command, State, Length and Data to calculate the checksum.
+    :param sum_of_bytes:
+    :return checksum:
     """
     checksum = 0xFF - sum_of_bytes
     return checksum
