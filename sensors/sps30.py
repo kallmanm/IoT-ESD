07c24c55de4eca83facd02be7f34d03de603871d
Mattias Kallman <33825140+kallmanm@users.noreply.github.com>
2021-01-13 10:52:16 +0200

    added debug checks to sps30 class

--- sensors/sps30.py
+++ sensors/sps30.py
@@ -61,7 +61,7 @@ class Sps30:
         if self.debug:
             raw_response = self.ser.read(7)
             response = self.byte_unstuffing(raw_response)
-            print(f'Response Status: {response[3]}')
+            print(f'Response Status start_measurement(): {response[3]}')
             # use struct to unpack if not readable
         time.sleep(30)  # Minimum time needed to boot up the sensor.
 
@@ -73,7 +73,7 @@ class Sps30:
         if self.debug:
             raw_response = self.ser.read(7)
             response = self.byte_unstuffing(raw_response)
-            print(f'Response Status: {response[3]}')
+            print(f'Response Status stop_measurement(): {response[3]}')
             # use struct to unpack if not readable
 
     def read_measured_values(self, mode='float'):
@@ -98,8 +98,11 @@ class Sps30:
         raw_data = self.ser.read(data_to_read)
 
         unstuffed_raw_data = self.byte_unstuffing(raw_data)  # Unstuffing the raw_data.
-        error_flag = unstuffed_raw_data[3]
-        print(f'error-flag: {error_flag}')
+
+        if self.debug:
+            error_flag = unstuffed_raw_data[3]
+            print(f'Response Status read_measured_values(): {error_flag}')
+
         # Datasheet 5.2: Figure 4 MISO Frame.
         rx_data = unstuffed_raw_data[5:-2]  # Removing header and tail bits.
 
@@ -123,7 +126,12 @@ class Sps30:
         Datasheet 5.3.4
         """
         self.ser.write([0x7E, 0x00, 0x10, 0x00, 0xEF, 0x7E])
-        # TODO: add self.ser.read() functionality to read response.
+
+        if self.debug:
+            raw_response = self.ser.read(7)
+            response = self.byte_unstuffing(raw_response)
+            print(f'Response Status sleep(): {response[3]}')
+            # use struct to unpack if not readable
 
     def wake_up(self):
         """
@@ -131,13 +139,24 @@ class Sps30:
         """
         self.ser.write([0xFF])
         self.ser.write([0x7E, 0x00, 0x11, 0x00, 0xEE, 0x7E])
-        # TODO: add self.ser.read() functionality to read response.
+
+        if self.debug:
+            raw_response = self.ser.read(7)
+            response = self.byte_unstuffing(raw_response)
+            print(f'Response Status wake_up(): {response[3]}')
+            # use struct to unpack if not readable
 
     def start_fan_cleaning(self):
         """
         Datasheet 5.3.6
         """
         self.ser.write([0x7E, 0x00, 0x56, 0x00, 0xA9, 0x7E])
+
+        if self.debug:
+            raw_response = self.ser.read(7)
+            response = self.byte_unstuffing(raw_response)
+            print(f'Response Status start_fan_cleaning(): {response[3]}')
+            # use struct to unpack if not readable
 
     def read_write_auto_cleaning_interval(self):
         """
@@ -157,6 +176,7 @@ class Sps30:
         ser_num = 0x03
         self.ser.write([0x7E, 0x00, 0xD0, 0x01, 0x00, 0x2E, 0x7E])
         # TODO: add self.ser.read() functionality to read response.
+        # TODO: add self.debug check to confirm error status flag
 
     def read_version(self):
         """
@@ -164,6 +184,7 @@ class Sps30:
         """
         self.ser.write([0x7E, 0x00, 0xD1, 0x00, 0x2E, 0x7E])
         # TODO: add self.ser.read() functionality to read response.
+        # TODO: add self.debug check to confirm error status flag
 
     def read_device_status_register(self):
         """
@@ -171,12 +192,19 @@ class Sps30:
         """
         self.ser.write([0x7E, 0x00, 0xD2, 0x01, 0x00, 0x2C, 0x7E])
         # TODO: add self.ser.read() functionality to read response.
+        # TODO: add self.debug check to confirm error status flag
 
     def device_reset(self):
         """
         Datasheet 5.3.11
         """
         self.ser.write([0x7E, 0x00, 0xD3, 0x00, 0x2C, 0x7E])
+
+        if self.debug:
+            raw_response = self.ser.read(7)
+            response = self.byte_unstuffing(raw_response)
+            print(f'Response Status device_reset(): {response[3]}')
+            # use struct to unpack if not readable
 
     def open_port(self):
         """
