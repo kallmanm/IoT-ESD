d0fc59f0c6e3257dee7349be5590734386e43165
Mattias Kallman <33825140+kallmanm@users.noreply.github.com>
2021-02-15 16:20:18 +0200

    rm f str in test sps30

--- test_sps30.py
+++ test_sps30.py
@@ -6,114 +6,118 @@ import time
 
 device_port = "/dev/ttyUSB0"
 COUNT = 20
-print(f'Device port set to: {device_port}')
-
-print(f'******************')
+print('Device port set to:')
+print(device_port)
+print('******************')
 
 sensor_sps30 = sps30.Sps30(device_port)
 
-print(f'Sps30 __init__(): {sensor_sps30.ser.is_open}')
-
-print(f'----------')
+print('Sps30 __init__():')
+print(sensor_sps30.ser.is_open)
+print('----------')
 print('Device information - product type:')
 dipt = sensor_sps30.device_information()
 print(dipt)
 
 
-print(f'----------')
-print(f'Testing close_port()')
+print('----------')
+print('Testing close_port()')
 sensor_sps30.close_port()
-print(f'Port Status: {sensor_sps30.ser.is_open}')
-print(f'Should be False\n')
+print('Port Status:')
+print(sensor_sps30.ser.is_open)
+print('Should be False\n')
 
-print(f'----------')
-print(f'Testing open_port()')
+print('----------')
+print('Testing open_port()')
 sensor_sps30.open_port()
-print(f'Port Status: {sensor_sps30.ser.is_open}')
-print(f'Should be True\n')
+print('Port Status:')
+print(sensor_sps30.ser.is_open)
+print('Should be True\n')
 
 
-print(f'----------')
+print('----------')
 print('Device information - serial number:')
 di2 = sensor_sps30.device_information(return_info='serial_number')
-print(f'{di2}\n')
-print(f'******************')
+print(di2)
+print('******************')
 
 
 # FLOAT MODE
-print(f'******************')
+print('******************')
 # Test start_measurement() default mode -> float values
 # start_up_time recommended values depending on particle air values: 8,16 or 30 seconds.
-print(f'----------')
-print(f'Test start_measurement() default mode -> float values')
+print('----------')
+print('Test start_measurement() default mode -> float values')
 sensor_sps30.start_measurement(start_up_time=8)
 print()
 
 # Test read_measured_values() default mode -> float values
-print(f'----------')
-print(f'Test read_measured_values() default mode -> float values')
+print('----------')
+print('Test read_measured_values() default mode -> float values')
 for i in range(COUNT):
     data_readout = sensor_sps30.read_measured_values()
-    print(f'data output number {i+1}: {data_readout}')
+    print('data output number' + i+1+':' + data_readout)
     print()
     time.sleep(1)
 print()
 
 # Test stop_measurement()
-print(f'----------')
-print(f'Test stop_measurement()')
+print('----------')
+print('Test stop_measurement()')
 sensor_sps30.stop_measurement()
 
 # INTEGER MODE
-print(f'******************')
+print('******************')
 
 # Test start_measurement() mode -> integer values
-print(f'----------')
-print(f'Test start_measurement() default mode -> integer values')
+print('----------')
+print('Test start_measurement() default mode -> integer values')
 sensor_sps30.start_measurement(mode='integer', start_up_time=8)
 print()
 
 # Test read_measured_values() mode -> integer values
-print(f'----------')
-print(f'Test read_measured_values() default mode -> integer values')
+print('----------')
+print('Test read_measured_values() default mode -> integer values')
 for i in range(COUNT):
     data_readout = sensor_sps30.read_measured_values(mode='integer')
-    print(f'data output number {i}: {data_readout}')
+    print('data output number'  + i+1 + ':' + data_readout)
     time.sleep(1)
 print()
 
 # Test stop_measurement()
-print(f'----------')
-print(f'Test stop_measurement()')
+print('----------')
+print('Test stop_measurement()')
 sensor_sps30.stop_measurement()
 
-print(f'******************')
+print('******************')
 # Sleep -> Wake up -> Start fan cleaning
 sleep = sensor_sps30.sleep()
-print(f'sleep - {sleep}')
+print('sleep - ')
+print(sleep)
 print('sleeping 4 seconds...')
 time.sleep(4)
 wk = sensor_sps30.wake_up()
-print(f'wake_up - {wk}')
+print('wake_up - ')
+print(wk)
 
 
 sensor_sps30.start_measurement()
 sfc = sensor_sps30.start_fan_cleaning()
-print(f'start_fan_clean - {sfc}')
+print('start_fan_clean - ' + sfc)
 print('sleeping 7 seconds...')
 time.sleep(7)
 sensor_sps30.stop_measurement()
 
-print(f'******************')
+print('******************')
 rd = sensor_sps30.read_version()
-print(f'read_version - {rd}')
+print('read_version -' + rd)
 dr = sensor_sps30.device_reset()
-print(f'device_reset - {dr}')
+print('device_reset - {dr}')
 
-print(f'******************')
+print('******************')
 sensor_sps30.start_measurement(start_up_time=8)
 rdsr = sensor_sps30.read_device_status_register()
-print(f'{rdsr}')
+print(rdsr)
 print('sleeping 3 seconds...')
 time.sleep(3)
 sensor_sps30.stop_measurement()
